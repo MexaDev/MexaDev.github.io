2a456e6c36dc691e7c4ff7785cc2912b851ceea0
MexaDev <38158308+MexaDev@users.noreply.github.com>
2022-11-15 13:48:13 +0200

    Initial Upload
    Initial upload for remote support

---
+++ Redesign/js/platter-viewer.js
@@ -0,0 +1,168 @@
+// // Get the modal
+var modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// //Button variables
+// const btn1 = document.getElementById("item1");
+// const btn2 = document.getElementById("item2");
+// const btn3 = document.getElementById("item3");
+
+// //Description variables
+// const desc1 = document.getElementById("description1")
+// const desc2 = document.getElementById("description2")
+// const desc3 = document.getElementById("description3")
+
+//Display Function Declarations
+function displayInfo1(){
+  var a = document.getElementById("description1");
+  a.classList.toggle("display")
+}
+function displayInfo2(){
+  var a = document.getElementById("description2");
+  a.classList.toggle("display")
+  // console.log("Opened")
+}
+function displayInfo3(){
+  var a = document.getElementById("description3");
+  a.classList.toggle("display")
+}
+function displayInfo4(){
+  var a = document.getElementById("description4");
+  a.classList.toggle("display")
+}
+function displayInfo5(){
+  var a = document.getElementById("description5");
+  a.classList.toggle("display")
+}
+function displayInfo6(){
+  var a = document.getElementById("description6");
+  a.classList.toggle("display")
+}
+function displayInfo7(){
+  var a = document.getElementById("description7");
+  a.classList.toggle("display")
+}
+function displayInfo8(){
+  var a = document.getElementById("description8");
+  a.classList.toggle("display")
+}
+function displayInfo9(){
+  var a = document.getElementById("description9");
+  a.classList.toggle("display")
+}
+function displayInfo10(){
+  var a = document.getElementById("description10");
+  a.classList.toggle("display")
+}
+function displayInfo11(){
+  var a = document.getElementById("description11");
+  a.classList.toggle("display")
+}
+function displayInfo12(){
+  var a = document.getElementById("description12");
+  a.classList.toggle("display")
+}
+function displayInfo13(){
+  var a = document.getElementById("description13");
+  a.classList.toggle("display")
+}
+function displayInfo14(){
+  var a = document.getElementById("description14");
+  a.classList.toggle("display")
+}
+function displayInfo15(){
+  var a = document.getElementById("description15");
+  a.classList.toggle("display")
+}
+function displayInfo16(){
+  var a = document.getElementById("description16");
+  a.classList.toggle("display")
+}
+
+//Close Function Declarations
+function closeInfo1(){
+  let a = document.getElementById("description1");
+  a.classList.toggle("display");
+}
+function closeInfo2(){
+  let a = document.getElementById("description2");
+  a.classList.toggle("display");
+}
+function closeInfo3(){
+  let a = document.getElementById("description3");
+  a.classList.toggle("display");
+}
+function closeInfo4(){
+  let a = document.getElementById("description4");
+  a.classList.toggle("display");
+}
+function closeInfo5(){
+  let a = document.getElementById("description5");
+  a.classList.toggle("display");
+}
+function closeInfo6(){
+  let a = document.getElementById("description6");
+  a.classList.toggle("display");
+}
+function closeInfo7(){
+  let a = document.getElementById("description7");
+  a.classList.toggle("display");
+}
+function closeInfo8(){
+  let a = document.getElementById("description8");
+  a.classList.toggle("display");
+}
+function closeInfo9(){
+  let a = document.getElementById("description9");
+  a.classList.toggle("display");
+}
+function closeInfo10(){
+  let a = document.getElementById("description10");
+  a.classList.toggle("display");
+}
+function closeInfo11(){
+  let a = document.getElementById("description11");
+  a.classList.toggle("display");
+}
+function closeInfo12(){
+  let a = document.getElementById("description12");
+  a.classList.toggle("display");
+}
+function closeInfo13(){
+  let a = document.getElementById("description13");
+  a.classList.toggle("display");
+}
+function closeInfo14(){
+  let a = document.getElementById("description14");
+  a.classList.toggle("display");
+}
+function closeInfo15(){
+  let a = document.getElementById("description15");
+  a.classList.toggle("display");
+}
+function closeInfo16(){
+  let a = document.getElementById("description16");
+  a.classList.toggle("display");
+}
