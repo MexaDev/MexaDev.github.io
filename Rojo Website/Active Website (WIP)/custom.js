7ef58e1d1d58cab9af138113d4f99f606acb3242
MexaDev <38158308+MexaDev@users.noreply.github.com>
2023-03-09 11:11:59 +0200

    Rojo
    Website update

---
+++ Rojo Website/Active Website (WIP)/custom.js
@@ -0,0 +1,5 @@
+function test(){
+	let a = document.getElementById("test");
+	a.classList.toggle("image")
+	console.log("click")
+}
---
+++ Rojo Website/Active Website (WIP)/js/menu-viewer.js
@@ -0,0 +1,48 @@
+// // Get the modal
+var modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// //Button variables
+// const btn1 = document.getElementById("item1");
+// const btn2 = document.getElementById("item2");
+// const btn3 = document.getElementById("item3");
+
+// //Description variables
+// const desc1 = document.getElementById("description1")
+// const desc2 = document.getElementById("description2")
+// const desc3 = document.getElementById("description3")
+
+//Display Function Declarations
+function displayInfo1(){
+  let a = document.getElementById("description1");
+  a.classList.toggle("display")
+  console.log(ACTION)
+}
+
+//Close Function Declarations
+function closeInfo1(){
+  let a = document.getElementById("description1");
+  a.classList.toggle("display");
+}
